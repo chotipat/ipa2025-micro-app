0d354ee42cea67e137a6df55d8a774d5b8df93b7
Chotipat Pornavalai <chotipat@it.kmitl.ac.th>
2025-07-25 18:35:04 +0700

    Add scheduler send_task.py and RabbitMQ service

---
+++ scheduler/send_task.py
@@ -0,0 +1,10 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+channel.queue_declare(queue="router_jobs")
+
+channel.basic_publish(exchange="", routing_key="router_jobs", body="192.168.1.1")
+print("Sent job for router 192.168.1.1")
+connection.close()
